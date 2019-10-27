3cb93eb7cf1a91399bbf4be7b4100d7313892647
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-27 14:57:05 +0100

    using template engines with express

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 //Create route
 const express = require('express')
+const path = require('path')
 
+//Init App
 const app = express()
 
+//Load View Engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+//Home route
 app.get('/', function (req, res) {
-  res.send('Hello world')
+  res.render('index')
 })
 
+//Start server
 app.listen(3000, function () {
   console.log('Server stared on port 3000')
 })
